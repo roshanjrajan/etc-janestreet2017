010ae0e1a5b68210a172d48e0443371a339ca203
Neil <neil123@gmail.com>
2017-09-23 14:01:35 -0500

    testing Valbz

--- bot.py
+++ bot.py
@@ -5,9 +5,7 @@ import sys
 import socket
 import json
 import random
-vale_list=[]
-valbz_list=[]
-fp_days=120
+
 def connect():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("test-exch-DATAGODS", 25000))
@@ -20,31 +18,23 @@ def write(exchange, obj):
 def read(exchange):
     return json.loads(exchange.readline())
 
-def vale_strat(priceVale):
-    vale_list.append(priceVale)
-    if(len(vale_list) >= fp_days):
-        vale_list.pop(0)
-    
-    fair_vale = sum(vale_list)/len(vale_list)
-    print(fair_vale)
-    if(priceVale<fair_vale):
-        return(1)
-    elif(priceVale>fair_vale):
-        return(0)
+def submit(t_t, symbol, orderType, price, number):
+	orderID = random.randint(1, 10*10)
+	write(exchange, {"type": t_t, "order_id": orderID, "symbol": symbol, "dir": orderType, "price": price, "size": number})
 
-def valbz_strat(priceValbz):
-    valbz_list.append(priceValbz)	
-    if(len(valbz_list) >= fp_days):
-	valbz_list.pop(0)
-    fair_valbz = sum(valbz_list)/len(valbz_list)
-    print(fair_valbz)
-    if(priceValbz<fair_valbz):
+def trash_strat(priceVale, numberVale, priceValbz, numberValbz):
+    can = priceVale * numberVale + 10
+    number=min(numberValbz,numberVale)
+    bz_value = priceValbz * number - 10
+    vale_value = priceVale * number
+    if vale_value>bz_value:
         return(1)
-    elif(priceValbz>fair_valbz):
+    elif bz_value>vale_value:
         return(0)
-
+    return(23)
 
 def main():
+    v_tran = 0
     exchange = connect()
     write(exchange, {"type": "hello", "team": "DATAGODS"})
     hello_from_exchange = read(exchange)
@@ -68,42 +58,40 @@ def main():
             #            if bond_counter>number_being_sold:
             #                write(exchange, {"type": "add", "order_id": orderID, "symbol": "BOND", "dir": "SELL", "price": buy_order[0], "size": number_being_sold})
             if symbol =='VALBZ':
-		print(message)
-		if len(message['sell']):
+                if len(message['sell']):
                     VALBZ_price=message['sell'][0][0]
                     VALBZ_number=message['sell'][0][1]
-                    if valbz_strat(VALBZ_price):
-			print('VALBZ_BUY')
-                        orderID=random.randint(1, 10**5)
-                        print({"type": "add", "order_id": orderID, "symbol": "VALBZ", "dir": "BUY", "price": VALBZ_price, "size": VALBZ_number})
-			write(exchange, {"type": "add", "order_id": orderID, "symbol": "VALBZ", "dir": "BUY", "price": VALBZ_price, "size": VALBZ_number})
-
+                    v_tran += 1
 
             if symbol == 'VALE':
-		print(message)
                 if len(message['sell']):
-		    VALE_price=message['sell'][0][0]
+		            VALE_price=message['sell'][0][0]
                     VALE_number=message['sell'][0][1]
-                    if vale_strat(VALE_price):
-                        print('VALEBUY')
-                        orderID=random.randint(1, 10**5)
-                        print( {"type": "add", "order_id": orderID, "symbol": "VALE", "dir": "BUY", "price": VALE_price, "size": VALE_number})
-			write(exchange, {"type": "add", "order_id": orderID, "symbol": "VALE", "dir": "BUY", "price": VALE_price, "size": VALE_number})
+                    v_tran +=1
 
-
-            #if ValBZ_strat(priceVal,priceValb,numberVal,numberValb)== 1 :
-            #    write(exchange, {"type": "add", "order_id": orderID, "symbol": "VALBZ", "dir": "BUY", "price": sell_order[0], "size": sell_order[1]})
-            #    write(exchange, {"type": "add", "order_id": orderID, "symbol": "VALE", "dir": "BUY", "price": sell_order[0], "size": sell_order[1]})
-
+            if(v_tran = 2)
+                v_trade=trash_strat(VALE_price,VALE_number,VALBZ_price,VALBZ_number)
+                if v_trade==1:
+                    submit('add','VALBZ', 'BUY', VALBZ_price, min(VALBZ_number,VALE_number)
+                    submit('add','VALE', 'SELL', VALE_price, min(VALBZ_number,VALE_number)
+                elif v_trade==0:
+                    submit('add','VALE', 'BUY', VALE_price, min(VALBZ_number,VALE_number)
+                    submit('add','VALBZ', 'SELL', VALBZ_price, min(VALBZ_number,VALE_number)
+                v_tran = 0
 
         if type_of_order == 'fill':
-		if message['symbol']=='VALBZ' or message['symbol']=='VALE':
-                	if message['dir']== 'BUY':
-                    		print(message)
-				bond_counter=bond_counter+int(message['size'])
-                	if message['dir'] == 'SELL':
-				print(message)
-                    		bond_counter=bond_counter-int(message['size'])
+            if message['symbol']=='BOND':
+                if message['dir']== 'BUY':
+                    print(message)
+				    bond_counter=bond_counter+int(message['size'])
+                if message['dir'] == 'SELL':
+				    print(message)
+                    bond_counter=bond_counter-int(message['size'])
+
+		    if message['symbol']=='VALBZ' or message['symbol']=='VALE':
+                if message['dir']== 'BUY':
+                    print(message)
+                     write(exchange, {"type": 'convert', "order_id":random.randint(1, 10**5) , "symbol": message['symbol'], "dir": "SELL", "size": message['size']})
 	if type_of_order=='ack':
 		print(message)
 	if type_of_order=='reject':
