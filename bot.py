015fc62f2f7fcbafb2193e69f26a14ed65a50932
Neil <neil123@gmail.com>
2017-09-23 11:41:17 -0500

    Testing bondcounter

--- bot.py
+++ bot.py
@@ -23,6 +23,7 @@ def main():
     write(exchange, {"type": "hello", "team": "DATAGODS"})
     hello_from_exchange = read(exchange)
     print("The exchange replied:", hello_from_exchange, file=sys.stderr)
+    bond_counter=0
     while(1):
         message = read(exchange)
         print(message)
@@ -35,8 +36,17 @@ def main():
                     if int(sell_order[0])<1000:
                             orderID=random.randint(1, 10**5)
                             write(exchange, {"type": "add", "order_id": orderID, "symbol": "BOND", "dir": "BUY", "price": sell_order[0], "size": sell_order[1]})
-)
-
-
+                for buy_order in message['buy']:
+                    if int(buy_order[0])>=1002:
+                        orderID=random.randint(1, 10**5)
+                        number_being_sold=buy_order[1]
+                        if bond_counter>number_being_sold:
+                            write(exchange, {"type": "add", "order_id": orderID, "symbol": "BOND", "dir": "SELL", "price": buy_order[0], "size": number_being_sold})
+        if type_of_order == 'fill':
+            if message[symbol]='BOND':
+                if message['dir'] = 'BUY':
+                    bond_counter=bond_counter+int(message['size'])
+                if message['dir'] = 'SELL':
+                    bond_counter=bond_counter-int(message['size'])
 if __name__ == "__main__":
     main()
